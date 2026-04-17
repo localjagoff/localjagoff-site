843217ef43b8ca051354978d5228ced17d5902ed
localjagoff <localjagoff@gmail.com>
2026-04-17 00:07:08 -0400

    get-products.js

--- api/get-products.js
+++ api/get-products.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://api.printful.com/store/products?store_id=18032822', {
-  headers: {
-    'Authorization': `Bearer ${process.env.PRINTFUL_API_KEY}`
-  }
-});
+  const response = await fetch('https://api.printful.com/store/products/428550417?store_id=18032822', {
+    headers: {
+      'Authorization': `Bearer ${process.env.PRINTFUL_API_KEY}`
+    }
+  });
 
   const data = await response.json();
 
