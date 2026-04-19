9735cd5d16af3c5348773c52b6a0b10cdf5da0e1
localjagoff <localjagoff@gmail.com>
2026-04-18 23:13:02 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* 🔥 GOOGLE FONTS */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
