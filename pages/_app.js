f9a0526fb3ed0f87432267147595df750098b862
localjagoff <localjagoff@gmail.com>
2026-04-25 23:34:58 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,66 @@
-import '../styles/global.css';
-import Head from 'next/head';
+import "../styles/global.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isProductPage = router.pathname === "/product/[id]";
+
   return (
     <>
       <Head>
         <title>Local Jagoff</title>
-        <meta name="description" content="Pittsburgh attitude. No corporate nonsense." />
+        <meta
+          name="description"
+          content="Pittsburgh attitude. No corporate nonsense."
+          key="description"
+        />
 
-        {/* Open Graph */}
-        <meta property="og:title" content="Local Jagoff" />
-        <meta property="og:description" content="Pittsburgh attitude. No corporate nonsense." />
-        <meta property="og:image" content="https://www.localjagoff.com/images/banner.jpg" />
-        <meta property="og:url" content="https://www.localjagoff.com" />
-        <meta property="og:type" content="website" />
+        {!isProductPage && (
+          <>
+            <meta property="og:title" content="Local Jagoff" key="og:title" />
+            <meta
+              property="og:description"
+              content="Pittsburgh attitude. No corporate nonsense."
+              key="og:description"
+            />
+            <meta
+              property="og:image"
+              content="https://www.localjagoff.com/images/banner.jpg"
+              key="og:image"
+            />
+            <meta
+              property="og:url"
+              content="https://www.localjagoff.com"
+              key="og:url"
+            />
+            <meta property="og:type" content="website" key="og:type" />
 
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Local Jagoff" />
-        <meta name="twitter:description" content="Pittsburgh attitude. No corporate nonsense." />
-        <meta name="twitter:image" content="https://www.localjagoff.com/images/banner.jpg" />
+            <meta
+              name="twitter:card"
+              content="summary_large_image"
+              key="twitter:card"
+            />
+            <meta
+              name="twitter:title"
+              content="Local Jagoff"
+              key="twitter:title"
+            />
+            <meta
+              name="twitter:description"
+              content="Pittsburgh attitude. No corporate nonsense."
+              key="twitter:description"
+            />
+            <meta
+              name="twitter:image"
+              content="https://www.localjagoff.com/images/banner.jpg"
+              key="twitter:image"
+            />
+          </>
+        )}
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/icon.png" />
       </Head>
 
       <Component {...pageProps} />
