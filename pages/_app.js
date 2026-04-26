a297f7fe40645dde55354016765317110b5410c7
localjagoff <localjagoff@gmail.com>
2026-04-25 20:50:23 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,25 @@
-import "../styles/global.css";
-import Head from "next/head";
+import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Local Jagoff</title>
-        <meta
-          name="description"
-          content="Certified nonsense. Pittsburgh attitude."
-          key="description"
-        />
+        <meta name="description" content="Pittsburgh attitude. No corporate nonsense." />
 
-        <meta property="og:title" content="Local Jagoff" key="og:title" />
-        <meta
-          property="og:description"
-          content="Certified nonsense. Pittsburgh attitude."
-          key="og:description"
-        />
-        <meta
-          property="og:image"
-          content="https://www.localjagoff.com/images/banner.png"
-          key="og:image"
-        />
-        <meta
-          property="og:url"
-          content="https://www.localjagoff.com"
-          key="og:url"
-        />
-        <meta property="og:type" content="website" key="og:type" />
+        {/* Open Graph */}
+        <meta property="og:title" content="Local Jagoff" />
+        <meta property="og:description" content="Pittsburgh attitude. No corporate nonsense." />
+        <meta property="og:image" content="https://www.localjagoff.com/images/banner.jpg" />
+        <meta property="og:url" content="https://www.localjagoff.com" />
+        <meta property="og:type" content="website" />
 
-        <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
-        <meta
-          name="twitter:image"
-          content="https://www.localjagoff.com/images/banner.png"
-          key="twitter:image"
-        />
-
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/images/icon.png" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Local Jagoff" />
+        <meta name="twitter:description" content="Pittsburgh attitude. No corporate nonsense." />
+        <meta name="twitter:image" content="https://www.localjagoff.com/images/banner.jpg" />
       </Head>
 
       <Component {...pageProps} />
