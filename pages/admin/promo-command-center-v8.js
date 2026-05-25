d9f836ee0d7e85522e6d1ac3a682a6b1ee036c3c
localjagoff <localjagoff@gmail.com>
2026-05-25 15:13:26 -0400

    Add promo admin nav wrapper for generator

---
+++ pages/admin/promo-command-center-v8.js
@@ -0,0 +1,11 @@
+import PromoAdminNav from "../../components/PromoAdminNav";
+import PromoCommandCenterV7 from "./promo-command-center-v7";
+
+export default function PromoCommandCenterV8() {
+  return (
+    <>
+      <PromoAdminNav />
+      <PromoCommandCenterV7 />
+    </>
+  );
+}
