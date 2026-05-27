dcd43fabbfff80f69e606f8dda4522f5663f6a2c
localjagoff <localjagoff@gmail.com>
2026-05-26 20:53:13 -0400

    Redirect promo generator to clean builder

--- pages/admin/promo-generator.js
+++ pages/admin/promo-generator.js
@@ -1,1 +1,12 @@
-export { default } from "./promo-command-center";
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/admin/promo-builder",
+      permanent: false,
+    },
+  };
+}
+
+export default function PromoGeneratorRedirect() {
+  return null;
+}
