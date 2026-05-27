2a5d49955888c0b80d40815509a9b558c4f1caf1
localjagoff <localjagoff@gmail.com>
2026-05-26 21:18:15 -0400

    Restore promo generator route

--- pages/admin/promo-generator.js
+++ pages/admin/promo-generator.js
@@ -1,12 +1,1 @@
-export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: "/admin/promo-builder",
-      permanent: false,
-    },
-  };
-}
-
-export default function PromoGeneratorRedirect() {
-  return null;
-}
+export { default } from "./promo-command-center";
