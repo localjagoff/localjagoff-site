d21710c8e8823b68426a53f4ba64eb7cf7199cf0
localjagoff <localjagoff@gmail.com>
2026-05-27 21:33:12 -0400

    Use Facebook Instagram focused promo studio page

--- pages/admin/promo-generator.js
+++ pages/admin/promo-generator.js
@@ -1,1 +1,13 @@
-export { default } from "./promo-command-center";
+import PromoAdminNav from "../../components/PromoAdminNav";
+import PromoStudioFbIgEnhancer from "../../components/PromoStudioFbIgEnhancer";
+import PromoCommandCenterBase from "./promo-command-center-v6";
+
+export default function PromoGenerator() {
+  return (
+    <>
+      <PromoAdminNav />
+      <PromoStudioFbIgEnhancer />
+      <PromoCommandCenterBase />
+    </>
+  );
+}
