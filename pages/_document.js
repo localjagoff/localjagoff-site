c066488ae0e862c5092840ccfb8c0af7fbc17ca1
Alex <localjagoff@gmail.com>
2026-09-06 23:52:34 -0400

    Add public Meta domain verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="facebook-domain-verification" content="b3lwec5fikx57f3umwspqydli1wj97" />
         {/* 🔥 GOOGLE FONTS */}
         <link
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&family=Inter:wght@300;400;500;600&display=swap"
